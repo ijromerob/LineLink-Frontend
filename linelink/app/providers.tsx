41282fe4cb30917e6dbb22f0e4248feafbd8bec6
Laurels Echichinwo <laurels.echichinwo@stu.cu.edu.ng>
2025-08-12 14:20:34 +0100

    remove unused next auth

--- linelink/app/providers.tsx
+++ linelink/app/providers.tsx
@@ -1,6 +1,5 @@
 
 
-import { SessionProvider } from 'next-auth/react';
 import { Provider } from 'react-redux';
 import store from '../store';
 import { AuthProvider } from '@/contexts/AuthContext';
